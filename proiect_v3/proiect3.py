d90ce8896ac417d31cf3d3dddb17bf717f987c9d
BiaGheorghe <61453241+BiaGheorghe@users.noreply.github.com>
2020-12-07 20:34:08 +0200

    Update 1
    Am introdus insert-ul in codul principal

--- proiect_v3/proiect3.py
+++ proiect_v3/proiect3.py
@@ -1,39 +1,40 @@
+import mysql.connector
 import re
 from requests import get
-import mysql.connector
 
-mydb = mysql.connector.connect(host="localhost", user="bia", passwd="bia2")
-print(mydb)
-if mydb:
-    print('Connection Successful')
+my_db = mysql.connector.connect(host="localhost", user="bia", passwd="bia2", database="tvseries")
+print(my_db)
+if my_db:
+    print('m am conectat')
 else:
-    print('Connection Unsuccesful')
-# from bs4 import BeautifulSoup as Soup
+    print('nu m am conectat')
+link_inp = input('provide a link: ')
+nota_imp = input('provide rhe score: ')
+last_episode_imp = input('provide the last episode: ')
+date_imp = input('data ultimei vizionari: ')
 
-# print(re.search('bc','abc').span()[0],', ',re.search('bc','abc').span()[1])
-link = input('introduceti link-ul dorit: ')
-
-# !!!!!! de verificat daca nota e intre 1 si 10
-nota = input('intoduceti nota: ')
-# !!!!! de verificat daca ultimul episod e <= decat nr total de episoade
-last_episode = input('ultimul episod vizionat: ')
-# !!!!!  de verificat daca data e valida si nu trece de data curenta
-date = input('data ultimei vizionari: ')
-
-url = get(link)
+url = get(link_inp)
 request = url.text
-
-# soup_data = Soup(request, 'html.parser')
-# movie = soup_data.find('div', {'class': 'title_wrapper'})
-# titlu = movie.h1.text
-# print(titlu)  # titlul este in interiorul unui h1
 
 pos_incep_titlu = re.search('<h1 class="">', url.text).span()[1]
 pos_sf_titlu = re.search('</h1>', url.text).span()[0] - 18  # eliminam caracterele de la titlu pana la </h1>
-title = request[pos_incep_titlu:pos_sf_titlu]
-print(title, nota)
+title_found = request[pos_incep_titlu:pos_sf_titlu]
+print(title_found, nota_imp)
 
 pos_incep_nr_ep = re.search('<span class="bp_sub_heading">', url.text).span()[1]
 pos_sf_nr_ep = re.search('episodes</span>', url.text).span()[0] - 1  # am eliminat spatiul de dupa nr
 nr_of_ep = request[pos_incep_nr_ep:pos_sf_nr_ep]
 print(nr_of_ep, 'episoade')
+
+
+my_cursor = my_db.cursor()
+
+sql_com = "INSERT INTO tvseriesandscore(title,link,score,nr_episodes,last_seen_episode,the_date,snoozed) "\
+          "VALUES (%s,%s,%s,%s,%s,%s,%s)"
+
+new_serial = [(title_found,link_inp,nota_imp,nr_of_ep,last_episode_imp,date_imp, 0)]
+
+my_cursor.executemany(sql_com, new_serial)
+
+my_db.commit()
+
