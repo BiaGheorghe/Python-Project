8e837b1a9a883ce8d67ad9838a9aebb8242a4b2d
BiaGheorghe <61453241+BiaGheorghe@users.noreply.github.com>
2020-12-26 20:40:42 +0200

    Update create_tb.py

--- proiect_v3/create_tb.py
+++ proiect_v3/create_tb.py
@@ -6,7 +6,8 @@ my_cursor = my_db.cursor()
 create_table_tvSeries = 'create table tvSeries_And_Score(id INT(255) UNSIGNED AUTO_INCREMENT PRIMARY KEY, '\
                         'title varchar(100), '\
                         'link varchar(254), score int(10), ' \
-                        'nr_episodes int(255) , last_seen_episode int(255),the_date date, snoozed varchar(4)) '
+                        'nr_episodes int(255) ,nr_seasons int(255), last_seen_episode varchar (7),the_date date, ' \
+                        'snoozed varchar(4)) '
 my_cursor.execute(create_table_tvSeries)
 show_tables = 'show tables'
 my_cursor.execute(show_tables)
