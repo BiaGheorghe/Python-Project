61263560768b0dfb31284b600af16978f2516a1e
BiaGheorghe <61453241+BiaGheorghe@users.noreply.github.com>
2020-12-26 16:20:51 +0200

    Update create_tb.py

--- proiect_v3/create_tb.py
+++ proiect_v3/create_tb.py
@@ -3,10 +3,10 @@ import mysql.connector
 my_db = mysql.connector.connect(host="localhost", user="bia", passwd="bia2", database="tvseries")
 
 my_cursor = my_db.cursor()
-create_table_tvSeries = 'create table tvSeriesAndScore(id INT(255) UNSIGNED AUTO_INCREMENT PRIMARY KEY, '\
-                        'title varchar(50), '\
-                        'link varchar(100), score int(10), ' \
-                        'nr_episodes int(255) , last_seen_episode int(255),the_date date, snoozed int(2)) '
+create_table_tvSeries = 'create table tvSeries_And_Score(id INT(255) UNSIGNED AUTO_INCREMENT PRIMARY KEY, '\
+                        'title varchar(100), '\
+                        'link varchar(254), score int(10), ' \
+                        'nr_episodes int(255) , last_seen_episode int(255),the_date date, snoozed varchar(4)) '
 my_cursor.execute(create_table_tvSeries)
 show_tables = 'show tables'
 my_cursor.execute(show_tables)
